a19c5dfab409f5595743aa8d0d3b42a2a2f5fd27
Nakaoni <16108885+Nakaoni@users.noreply.github.com>
2025-01-03 22:01:04 +0100

    GREEN - Add simple implementation to return client with a new ID

---
+++ src/main/java/fr/nakaoni/ineedmoney/Client/Client.java
@@ -0,0 +1,6 @@
+package fr.nakaoni.ineedmoney.Client;
+
+import java.util.UUID;
+
+public record Client(UUID id) {
+}
--- src/main/java/fr/nakaoni/ineedmoney/Client/ClientController.java
+++ src/main/java/fr/nakaoni/ineedmoney/Client/ClientController.java
@@ -4,12 +4,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 public class ClientController {
 
     @PostMapping("/clients")
-    ResponseEntity<String> registerNewClient() {
-        return ResponseEntity.created(null).body("");
+    ResponseEntity<Client> registerNewClient() {
+        return ResponseEntity.created(null).body(new Client(UUID.randomUUID()));
     }
 
 }

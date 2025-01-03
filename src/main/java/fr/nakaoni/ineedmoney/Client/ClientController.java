f754db678031562480caaa34e95654260c112efb
Nakaoni <16108885+Nakaoni@users.noreply.github.com>
2025-01-03 22:28:48 +0100

    GREEN - implement header location for new client

--- src/main/java/fr/nakaoni/ineedmoney/Client/ClientController.java
+++ src/main/java/fr/nakaoni/ineedmoney/Client/ClientController.java
@@ -3,8 +3,9 @@ package fr.nakaoni.ineedmoney.Client;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.UUID;
+import java.net.URI;
 
 @RestController
 public class ClientController {
@@ -12,7 +13,14 @@ public class ClientController {
     @PostMapping("/clients")
     ResponseEntity<Client> registerNewClient() {
         Client newClient = Client.register();
-        return ResponseEntity.created(null).body(newClient);
+
+        URI newClientLocation = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(newClient.id())
+                .toUri();
+
+        return ResponseEntity.created(newClientLocation).body(newClient);
     }
 
 }
